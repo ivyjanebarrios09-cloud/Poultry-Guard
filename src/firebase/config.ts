e1d4e31239180abed4bcff8f3fcec716c1463701
Ivy Jane <ivyjane033@gmail.com>
2025-10-22 19:58:39 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,4 @@
 // In a real app, you would initialize Firebase here and populate the config with your project's credentials.
 // For this sample, the configuration is managed by the Firebase Hosting environment.
 
-export const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{"apiKey":"AIzaSyC21T7_sde2-3432423423-23423423","authDomain":"studio-test-project.firebaseapp.com","projectId":"studio-test-project","storageBucket":"studio-test-project.appspot.com","messagingSenderId":"123123123","appId":"1:123123123:web:123123123123"}');
+export const firebaseConfig = {"apiKey":"AIzaSyC21T7_sde2-3432423423-23423423","authDomain":"studio-test-project.firebaseapp.com","projectId":"studio-test-project","storageBucket":"studio-test-project.appspot.com","messagingSenderId":"123123123","appId":"1:123123123:web:123123123123"};
